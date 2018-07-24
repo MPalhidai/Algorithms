d4083caeac1ecb86fd784535413f8bcafe1a02ac
Michael Palhidai <mpalhidai@gmail.com>
2018-07-24 11:40:31 -0700

    files

---
+++ learneroo/lib/graphs/graph.rb
@@ -0,0 +1,76 @@
+require_relative "../node"
+class Graph
+
+  attr_accessor :nodes
+
+  def initialize
+    @nodes = Hash.new
+  end
+
+  def add(node)
+    @nodes[node.data] = node
+  end
+
+  def fromArray(ar)
+    nodes.clear
+    ar.each_with_index do |val,i|
+      add(Node.new(i,val))
+    end
+  end
+
+  def bfs(start = 0, queue = [], output = [])
+
+    node = nodes[start]
+
+    unless node.visited
+      output.push(node.data)
+      node.connections.each {|conn| queue.push(conn) unless nodes[conn].visited }
+      node.visited = true
+    end
+
+    bfs(queue.shift(),queue, output) unless queue.empty?
+
+    output
+  end
+
+  def dfs(start = 0, output = [])
+    node = nodes[start]
+    output.push(node.data)
+    node.visited = true
+    node.connections.each {|conn| dfs(conn,output) unless nodes[conn].visited }
+
+    output
+  end
+
+  def oneConnected?
+    dfs.sort == nodes.keys.sort ? true : false
+  end
+
+  def hasCycle?(node = nodes.values.first, previous = nil, output = false)
+
+    node.visited = true
+
+    conns = node.connections.dup
+    conns.delete(previous)
+
+    conns.each do |conn|
+
+      if nodes[conn].visited
+        return true
+      else
+        output = hasCycle?(nodes[conn],node.data)
+      end
+
+    end
+
+    output
+
+  end
+
+  def unvisit_nodes
+    nodes.values.each do |node|
+      node.visited = false
+    end
+  end
+
+end
---
+++ learneroo/lib/lists/Queue.rb
@@ -0,0 +1,36 @@
+module LinkedList
+  class Node
+    attr_accessor :value, :next_node
+
+    def initialize(value, next_node)
+      @value = value
+      @next_node = next_node
+    end
+  end
+
+  class Queue
+    def initialize
+      @first = nil
+      @last = nil
+    end
+
+    def enqueue(value)
+      @first == nil ? @first = Node.new(value, @last) : @last = Node.new(value, @last)
+      #not working
+    end
+    alias_method :"<<", :enqueue
+
+    def dequeue
+      raise "Queue is empty" if is_empty?
+      value = @first.value
+      @first = @first.next_node
+      value
+    end
+
+    def is_empty?
+      @first.nil?
+    end
+  end
+end
+
+#Use ruby stack = LinkedList::Queue.new

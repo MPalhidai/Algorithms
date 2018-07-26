0c6bd03fc9a65139b74ce7de143bbf524cda709c
Michael Palhidai <mpalhidai@gmail.com>
2018-07-26 14:10:16 -0700

    scheduler

--- learneroo/lib/scheduler.rb
+++ learneroo/lib/scheduler.rb
@@ -2,40 +2,41 @@
 #
 # See if you can find an O(n log n) solution.
 
-class Event
-  attr_accessor :start, :finish, :overlaps, :id
+#what if sort by lowest start times and only choose the smallest diff then then kick the index to its finish time
 
-  def initialize(start = 0, finish = 0, overlaps = 0)
-    @start = start
-    @finish = finish
-    @overlaps = overlaps
+def scheduler(ar)
+  mergeSort(ar)
+
+end
+
+def mergeSort(ar)
+  if ar.length <= 1
+    ar
+  else
+    middle = (ar.length / 2).floor
+    left = mergeSort(ar[0...middle])
+    right = mergeSort(ar[middle..ar.length])
+    merge(left, right)
   end
 end
 
-#each tuple count number of overlaps and chose tuples with lowest counts until a tuple overlaps one already chosen
-
-def scheduler(ar)
-  schedule = []
-  idx = 0
-	ar.each_slice(2) do |start,finish|
-    timeslot = Event.new
-    timeslot.start = start
-    timeslot.finish = finish
-    timeslot.id = idx
-    idx += 1
-    schedule << timeslot
-	end
-  puts schedule[1].id
-  puts schedule[1].start
-  puts schedule[1].finish
-  puts schedule[1].overlaps
+def merge(left, right)
+  if left.empty?
+    right
+  elsif right.empty?
+    left
+  elsif left.first.first <= right.first.first
+    [left.first] + merge(left[1..left.length], right)
+  else
+    [right.first] + merge(left, right[1..right.length])
+  end
 end
 
-a = [4, 8, 1, 3, 7, 9, 5, 6]
+a = [[4, 8], [1, 3], [7, 9], [5, 6]]
 b = [3, 8, 1, 2, 3, 9, 1, 5, 4, 5, 8, 14]
 c = [0, 1, 14, 16, 4, 8, 10, 12, 7, 9, 8, 15, 3, 5]
 
-puts scheduler(a)
+print scheduler(a)
 
 # correct
 # [1, 3] [5, 6] [7, 9]

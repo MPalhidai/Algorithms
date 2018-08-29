419dc50f6c20e458cb2c1a5e2470e8dafb3d2ed3
Michael Palhidai <mpalhidai@gmail.com>
2018-08-29 15:11:46 -0700

    exercism bob solution

---
+++ exercism/javascript/bob/bob.js
@@ -0,0 +1,23 @@
+class Bob {
+  hey(message) {
+    message = message.trim();
+    let regexp_normal = /\w$|[.!]$/;
+    let regexp_q = /[?]$/;
+    let regexp_loud = /[A-Z]{4,}/;
+    let regexp_louder = /[A-Z]{2,}[!]$/;
+
+    if (regexp_loud.test(message) && regexp_q.test(message)) {
+      return "Calm down, I know what I\'m doing!";
+    } else if (regexp_loud.test(message) || regexp_louder.test(message)) {
+      return "Whoa, chill out!";
+    } else if (regexp_q.test(message)) {
+      return "Sure.";
+    } else if (regexp_normal.test(message)) {
+      return "Whatever.";
+    } else {
+      return "Fine. Be that way!";
+    }
+  };
+};
+
+export default Bob;

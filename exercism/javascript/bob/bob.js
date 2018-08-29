9e5ea36bd4f388ead203418271eb999ee0c65ec6
Michael Palhidai <mpalhidai@gmail.com>
2018-08-29 15:13:15 -0700

    refactor

--- exercism/javascript/bob/bob.js
+++ exercism/javascript/bob/bob.js
@@ -3,12 +3,11 @@ class Bob {
     message = message.trim();
     let regexp_normal = /\w$|[.!]$/;
     let regexp_q = /[?]$/;
-    let regexp_loud = /[A-Z]{4,}/;
-    let regexp_louder = /[A-Z]{2,}[!]$/;
+    let regexp_loud = /[A-Z]{4,}|[A-Z]{2,}[!]$/;
 
     if (regexp_loud.test(message) && regexp_q.test(message)) {
       return "Calm down, I know what I\'m doing!";
-    } else if (regexp_loud.test(message) || regexp_louder.test(message)) {
+    } else if (regexp_loud.test(message)) {
       return "Whoa, chill out!";
     } else if (regexp_q.test(message)) {
       return "Sure.";

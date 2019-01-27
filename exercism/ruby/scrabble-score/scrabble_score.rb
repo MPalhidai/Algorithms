e5d525c20daea6ded4a2ad6a75720469f383017b
Michael Palhidai <mpalhidai@gmail.com>
2019-01-26 18:08:15 -0800

    scrabble_score

--- exercism/ruby/scrabble-score/scrabble_score.rb
+++ exercism/ruby/scrabble-score/scrabble_score.rb
@@ -1,5 +1,5 @@
-module ExtendThroughInclude
-  @@scores = {
+class Scrabble
+  @@values = {
     1 => ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
     2 => ['D', 'G'],
     3 => ['B', 'C', 'M', 'P'],
@@ -9,55 +9,23 @@ module ExtendThroughInclude
     10 => ['Q', 'Z']
   }
 
-  def self.included(klass)
-    klass.extend(ClassMethods)
+  def initialize(string)
+    @string = string
+    @score = 0
   end
 
   def score
-    total = 0
-    return total unless @string
+    return @score unless @string
     @string.chars.each do |char|
       char.upcase!
-      @@scores.each do |k, v|
-        total += k if v.include?(char)
+      @@values.each do |k, v|
+        @score += k if v.include?(char)
       end
     end
-    total
+    @score
   end
 
-  module ClassMethods
-    @@scores = {
-      1 => ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-      2 => ['D', 'G'],
-      3 => ['B', 'C', 'M', 'P'],
-      4 => ['F', 'H', 'V', 'W', 'Y'],
-      5 => ['K'],
-      8 => ['J', 'X'],
-      10 => ['Q', 'Z']
-    }
-
-    def score(string)
-      total = 0
-      return total unless string
-      string.chars.each do |char|
-        char.upcase!
-        @@scores.each do |k, v|
-          total += k if v.include?(char)
-        end
-      end
-      total
-    end
+  def self.score(string)
+    Scrabble.new(string).score
   end
-end
-
-
-class Scrabble
-
-
-  include ExtendThroughInclude
-
-  def initialize(string)
-    @string = string
-  end
-
 end

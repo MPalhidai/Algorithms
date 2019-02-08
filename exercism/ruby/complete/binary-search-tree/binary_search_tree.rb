1795ffe6133cfd8c77e72eb98c586ef86fe655ba
Michael Palhidai <mpalhidai@gmail.com>
2019-02-08 11:25:26 -0800

    binary search tree

---
+++ exercism/ruby/complete/binary-search-tree/binary_search_tree.rb
@@ -0,0 +1,25 @@
+class Bst
+  attr_reader :data
+  attr_accessor :left, :right
+
+  def initialize(num = nil)
+    @data = num
+    @left = nil
+    @right = nil
+  end
+
+  def insert(num)
+    if num > data
+      right ? right.insert(num) : self.right = Bst.new(num)
+    else
+      left ? left.insert(num) : self.left = Bst.new(num)
+    end
+  end
+
+  def each(&block)
+    return enum_for(:each) unless block_given?
+    left.each(&block) if left
+    yield(data)
+    right.each(&block) if right
+  end
+end
